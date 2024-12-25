67da899370da3b038de8675d193e78906005b06b
GlitzyIsBored <dragonstudio209@gmail.com>
2024-12-26 00:21:09 +1100

    automatic age thing so i dont have to change this yearly

--- collapse.js
+++ collapse.js
@@ -1,10 +1,18 @@
+console.log('omg');
 console.log("Web first developed in 14/05/23. Last updated 24/08/24");
 
+//VARIABLES
 const collapse = document.getElementById("option");
 
+const birthDate = new Date("July 25, 2006");
+let toYear = birthDate.getFullYear();
+
+//NAVBAR
 collapse.addEventListener("click", action);
 function action() {
     document.getElementById("nav").classList.toggle('active');
     collapse.classList.toggle('click');
-
 }
+
+//AGE
+document.getElementById("omgAge").innerHTML = new Date().getFullYear() - toYear;
